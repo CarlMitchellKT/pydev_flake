45141f5ee6728384684a01e950d543b0baa7dada
Carl Mitchell <carl.mitchell@gomotive.com>
2024-12-19 10:35:03 -0500

    Add initial flake.nix for Python development

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    inputs @ { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      envWithScript = script:
+        (pkgs.buildFHSEnv {
+          name = "python-env";
+          targetPkgs = pkgs: (with pkgs; [
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+            pythonManylinuxPackages.manylinux2014Package
+            uv
+            cmake
+            ninja
+            gcc
+          ]);
+          runScript = "${pkgs.writeShellScriptBin "runScript" (''
+                    set -e
+                    test -d .venv || ${pkgs.uv}/bin/uv venv
+                    test -f .python-version || ${pkgs.uv}/bin/uv init .
+                    source .venv/bin/activate
+                    set +e
+                  ''
+            + script)}/bin/runScript";
+        }).env;
+    in
+    {
+      devShell = envWithScript "bash";
+    });
+}
